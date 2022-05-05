1476f01f872bd47775afa8d2ccf5a4152b0d6304
Lucas <lucasnethaj@protonmail.com>
2022-05-05 22:58:08 +0200

    Tilføj beskrivende kommentarer

--- opskrifter/models.py
+++ opskrifter/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
-
+# Hver class svarer til en tabel i vores sql database
+# Derfor nedarver den fra 'models.Model'
 class Opskrift(models.Model):
+
+    # Opskriften har to streng felter
     titel = models.CharField(max_length=100)
     instrukser = models.CharField(max_length=1000)
+
+    # En opskrift kan have mange forskellige ingrdienser
+    # Derfor dannes en relation til mange ingrdienser.
     ingredienser = models.ManyToManyField('Ingrediens',)
 
+    # Display værdien af table entry.
     def __str__(self):
         return self.titel
 
 
+# En tabel med alle de forskellige ingredienser,
+# som opskrifteren ellere brugerne referer til
 class Ingrediens(models.Model):
-    # antal = models.IntegerField()
     type = models.CharField(max_length=100)
 
     def __str__(self):
